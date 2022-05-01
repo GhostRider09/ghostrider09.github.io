d5f8e2139c5cd24a95c1417936b6f618c6f72268
ghostrider09 <andreysarapulov09@gmail.com>
2022-05-01 23:36:56 +0700

    new order page - step 2

--- js/_plugins.js
+++ js/_plugins.js
@@ -136,37 +136,3 @@
         };
     });
 })(jQuery);
-
-$(document).ready(function(){
-    $('._counter').counterField();
-    // $('._counter').trigger('switchDisabled', true);
-    // $('._counter').trigger('switchError', true);
-
-    $('body').on('click', '._switch-pass', function(e){
-        const $field = $(this).closest('.field').find('.field__element');
-
-        let bShowed = $(this).hasClass('__icon-eye-show');
-
-        if ( bShowed ) {
-            $(this).removeClass('__icon-eye-show').addClass('__icon-eye-hide');
-            $field.prop('type', 'password');
-        } else {
-            $(this).addClass('__icon-eye-show').removeClass('__icon-eye-hide');
-            $field.prop('type', 'text');
-        }
-        
-        e.preventDefault();
-    })
-    .on('click', '._show-main-menu', function(e){
-        $('._main-menu-area').addClass('__opened');
-        $('body').addClass('_open-window');
-
-        e.preventDefault;
-    })
-    .on('click', '._close', function(e){
-        $( $(this).data('target') ).removeClass('__opened');
-        $('body').removeClass('_open-window');
-
-        e.preventDefault;
-    });
-});
